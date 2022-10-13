e5afdafc4774ec7a75e9ffd1cbdf8bea4fb1b0ef
ninefloor <less0805@gmail.com>
2022-10-13 11:40:33 +0900

    CORS 설정

--- echo.js
+++ echo.js
@@ -20,6 +20,7 @@ http.createServer((request, response) => {
     // });
     // ? request 객체는 ReadableStream, response 객체는 WritableStream이기 때문에
     // ? 데이터를 한 스트림에서 다른 스트림으로 직접 연결하는 pipe를 사용할 수 있다.
+    response.setHeader("Access-Control-Allow-Origin", "*");
     request.pipe(response);
   } else {
     // ! 메소드가 POST 이면서 엔드포인트가 /echo가 아니라면 오류 코드 응답
